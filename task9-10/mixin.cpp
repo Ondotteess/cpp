6c852d2a5d2b69117008c27fcb0c530130f5fd7f
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-04-29 22:51:10 +0700

    Create mixin.cpp

---
+++ task9-10/mixin.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+template <typename Derived, int Limit>
+class LimitMixin {
+private:
+    static int instanceCount;
+
+protected:
+    LimitMixin() {
+        if (instanceCount >= Limit) {
+            throw std::runtime_error("Instance limit exceeded");
+        }
+        instanceCount++;
+    }
+
+public:
+    ~LimitMixin() {
+        instanceCount--;
+    }
+};
+
+template <typename Derived, int Limit>
+int LimitMixin<Derived, Limit>::instanceCount = 0;
+
+class Test : public LimitMixin<Test, 3> {
+public:
+    Test() {
+        std::cout << "MyClass instance created." << std::endl;
+    }
+};
+
+int main() {
+    try {
+        Test obj1;
+        Test obj2;
+        Test obj3;
+        Test obj4;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
