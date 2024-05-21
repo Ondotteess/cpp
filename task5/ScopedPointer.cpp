cf9755797b57015d29879acf96707638f9a54a93
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-05-22 04:23:24 +0700

    Update ScopedPointer.cpp

--- task5/ScopedPointer.cpp
+++ task5/ScopedPointer.cpp
@@ -6,43 +6,46 @@ private:
     T* ptr;
 
 public:
-    ScopedPointerDeep(T* p) : ptr(p) {}
+    ScopedPointerDeep(T* p = nullptr) : ptr(p) {}
 
     ~ScopedPointerDeep() {
         delete ptr;
     }
 
-    //copy
+    // copy
     ScopedPointerDeep(const ScopedPointerDeep& other) : ptr(nullptr) {
+        std::cout << "copy ctr called!\n";
         if (other.ptr) {
             ptr = new T(*other.ptr);
         }
     }
 
-    //move
-    ScopedPointerDeep(ScopedPointerDeep&& other): ptr(other.ptr) {
+    // move
+    ScopedPointerDeep(ScopedPointerDeep&& other) : ptr(other.ptr) {
+        std::cout << "move ctr called!\n";
         other.ptr = nullptr;
     }
 
-    //copy
-    ScopedPointerDeep& operator=(const ScopedPointerDeep& other) {
-        if (this != &other) {
-            delete ptr;
-            if (other.ptr) {
-                ptr = new T(*other.ptr);
-            }
-        }
+    // copy-and-swap 
+    ScopedPointerDeep& operator=(ScopedPointerDeep other) {
+        std::cout << "copy assigmnet oper!\n";
+        swap(*this, other);
         return *this;
     }
 
-    //move
-    ScopedPointerDeep& operator=(ScopedPointerDeep&& other) {
+    // move
+    ScopedPointerDeep& operator=(ScopedPointerDeep&& other) noexcept {
         if (this != &other) {
             delete ptr;
             ptr = other.ptr;
             other.ptr = nullptr;
         }
         return *this;
+    }
+
+    // redefine swap
+    void swap(ScopedPointerDeep& first, ScopedPointerDeep& second) noexcept {
+        std::swap(first.ptr, second.ptr);
     }
 
     T* operator->() const {
@@ -53,6 +56,7 @@ public:
         return *ptr;
     }
 };
+
 
 template<typename T>
 class ScopedPointerTransfer {
@@ -69,6 +73,12 @@ public:
     ScopedPointerTransfer(ScopedPointerTransfer&& other) : ptr(other.ptr) {
         other.ptr = nullptr;
     }
+
+    //deleted auto copy ctrs
+    ScopedPointerTransfer(const ScopedPointerTransfer&) = delete;
+
+    ScopedPointerTransfer& operator=(const ScopedPointerTransfer&) = delete;
+
 
     ScopedPointerTransfer& operator=(ScopedPointerTransfer&& other) {
         if (this != &other) {
