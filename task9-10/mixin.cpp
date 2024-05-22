af7523f0325b2c5c9813dd4a0d661d55c0694864
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-05-22 17:17:52 +0700

    copies

--- task9-10/mixin.cpp
+++ task9-10/mixin.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 template <typename Derived, int Limit>
 class LimitMixin {
@@ -7,6 +8,13 @@ private:
 
 protected:
     LimitMixin() {
+        if (instanceCount >= Limit) {
+            throw std::runtime_error("Instance limit exceeded");
+        }
+        instanceCount++;
+    }
+
+    LimitMixin(const LimitMixin&) {
         if (instanceCount >= Limit) {
             throw std::runtime_error("Instance limit exceeded");
         }
@@ -25,7 +33,22 @@ int LimitMixin<Derived, Limit>::instanceCount = 0;
 class Test : public LimitMixin<Test, 3> {
 public:
     Test() {
-        std::cout << "instance created." << std::endl;
+        std::cout << "Test instance created." << std::endl;
+    }
+
+    Test(const Test& other) : LimitMixin(other) {
+        std::cout << "Test instance copied." << std::endl;
+    }
+};
+
+class Test5 : public LimitMixin<Test5, 5> {
+public:
+    Test5() {
+        std::cout << "Test5 instance created." << std::endl;
+    }
+
+    Test5(const Test5& other) : LimitMixin(other) {
+        std::cout << "Test5 instance copied." << std::endl;
     }
 };
 
@@ -38,7 +61,30 @@ int main() {
     }
     catch (const std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
+        std::cout << "default ctr\n\n";
     }
 
+
+    try {
+        Test obj1;
+        Test obj2 = obj1;
+        Test obj3 = obj1;
+        Test obj4 = obj1;
+    }
+    catch (const std::exception& e) {
+        std::cout << "exception: " << e.what() << std::endl;
+        std::cout << "copy ctor\n";
+    }
+
+
+    try {
+        Test5 a;
+        Test5 b;
+        Test5 c;
+        Test5 d;
+    }
+    catch (const std::exception& e) {
+        std::cout << "You shouldn't see this\n";
+    }
     return 0;
 }
